35001838d7d9c6ff252a16da51dc16cf06f556fc
MarianoRuiz1 <marianojavierruiz01@gmail.com>
2023-11-06 19:59:50 -0300

    Clase 12

--- CoderApp/urls.py
+++ CoderApp/urls.py
@@ -4,10 +4,16 @@ from . import views
 
 app_name = 'CoderApp'
 
+
 urlpatterns = [
     path('fecha-actual/', views.mostrar_fecha_actual, name='mostrar_fecha_actual'),
-    path('productos/', views.producto_list, name='producto_list'),
-    path('index/', views.index, name='index'),
+    path('productos/', views.ProductoListView.as_view(), name='producto_list'),
     path('productos/agregar/', views.agregar_producto, name='agregar_producto'),
+    path('productos/editar/<int:pk>/', views.ProductoUpdateView.as_view(), name='editar_producto'),
+    path('productos/eliminar/<int:pk>/', views.ProductoDeleteView.as_view(), name='eliminar_producto'),
+    path('index/', views.index, name='index'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('register/', views.user_register, name='user_register'),
 ]
 
--- CoderApp/views.py
+++ CoderApp/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 import datetime
 from .models import Producto
 from .forms import ProductoForm
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def mostrar_fecha_actual(request):
@@ -12,6 +20,11 @@ def producto_list(request):
     productos = Producto.objects.all()
     print(productos)
     return render(request, 'producto_list.html', {'productos': productos})
+
+class ProductoListView(ListView):
+    model = Producto
+    template_name = 'producto_list.html'
+    context_object_name = 'productos'
 
 def index(request):
     return render(request, 'index.html')
@@ -25,3 +38,42 @@ def agregar_producto(request):
     else:
         form = ProductoForm()
     return render(request, 'producto_form.html', {'form': form})
+
+class ProductoUpdateView(UpdateView):
+    model = Producto
+    template_name = 'producto_form.html'
+    form_class = ProductoForm
+    success_url = reverse_lazy('CoderApp:producto_list')
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = 'producto_confirm_delete.html'
+    success_url = reverse_lazy('CoderApp:producto_list')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('CoderApp:index')
+        else:
+            # Manejo de error de inicio de sesión
+            return render(request, 'login.html', {'error': 'Nombre de usuario o contraseña incorrectos.'})
+    return render(request, 'login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('CoderApp:index')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('CoderApp:user_login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
